d8217b2d23c729b2dc30ff5c61b4b6ce1a91dc1b
Navidt <43656881+Navidt@users.noreply.github.com>
2024-04-03 17:16:01 -0500

    Got text input for speed working

--- UIHandler.ts
+++ UIHandler.ts
@@ -16,6 +16,8 @@ class UIHandler {
     this.playing = false
     this.engineRepetitions = 1
     this.queuedRepetitions = 1
+    let textField = document.getElementById("speedInput")! as HTMLInputElement
+    textField.addEventListener("focusout", this.textFieldChange)
   }
 
   togglePlay(button: HTMLButtonElement) {
@@ -32,16 +34,38 @@ class UIHandler {
     }
   }
 
+  getSpeedInverse(x: number) {
+    let s = 3
+    if (x >= 1) {
+      return (x - 1) / s
+    } else {
+      return Math.log(x) / s
+    }
+  }
+
   speedChange(slider: HTMLInputElement) {
-    // this.engineRepetitions = Math.floor(Number(slider.value))
     this.engineRepetitions = this.getSpeed(Number(slider.value))
-    let speedLabel = document.getElementById("speedLabel")! as HTMLParagraphElement
-    speedLabel.innerText = " Ticks/Frame: " + this.engineRepetitions.toFixed(2)
+    let speedLabel = document.getElementById("speedInput")! as HTMLInputElement
+    speedLabel.value = this.engineRepetitions.toFixed(2)
+  }
+
+  textFieldChange = (evt: FocusEvent) => {
+    let textField = evt.target as HTMLInputElement
+    let value = Number.parseFloat(textField.value)
+    if (Number.isNaN(value)) {
+      textField.value = this.engineRepetitions.toFixed(2)
+    } else {
+      console.log("Value: ", value)
+      this.engineRepetitions = value
+      let slider = document.getElementById("speedSlider")! as HTMLInputElement
+      slider.value = this.getSpeedInverse(value).toString()
+    }
   }
 
   draw = () => {
     this.renderer.render()
     if (this.playing) {
+      // Eventually we'll want to interpolate between frames when engineRepetitions < 1 to make it more smooth
       let n = Math.floor(this.queuedRepetitions)
       this.queuedRepetitions -= n
       for (let i = 0; i < n; i++) {
